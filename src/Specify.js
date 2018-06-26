a4c42e2c4d5d93e04772761c27ea818961cda60a
G Wright <wright.grayson@gmail.com>
2018-06-26 13:08:52 -0400

    Improve formatting for the specification table
    * Add a link to the Ruby language tutorial
    * wrap judgements code in `<code>` tags.
    
    See:
    
    * https://react-bootstrap-table.github.io/react-bootstrap-table2/<Paste>
    * https://react-bootstrap-table.github.io/react-bootstrap-table2/storybook/index.html?selectedKind=Work%20on%20Columns&selectedStory=Column%20Formatter&full=0&addons=1&stories=1&panelRight=0&addonPanel=storybook%2Factions%2Factions-panel
    * https://react-bootstrap-table.github.io/react-bootstrap-table2/storybook/index.html?selectedKind=Work%20on%20Header%20Columns&selectedStory=Column%20Formatter&full=0&addons=1&stories=1&panelRight=0&addonPanel=storybook%2Factions%2Factions-panel

--- src/Specify.js
+++ src/Specify.js
@@ -11,28 +11,11 @@ import Table from "react-bootstrap-table-next"
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
 class Specify extends React.Component {
-  removeFormatter = (cell, row) => (
-    <Button color="secondary" onClick={() => this.props.onRemoveSpec(cell)}>
-      Remove
-    </Button>
-  )
-
-  specColumns = [
-    { dataField: "partno", text: "Part" },
-    { dataField: "test_name", text: "Test" },
-    { dataField: "test_method", text: "Method" },
-    { dataField: "criteria", text: "Criteria" },
-    { dataField: "judgement", text: "Judgement Logic" },
-    { dataField: "created_by", text: "Created By" },
-    { dataField: "created_at", text: "Created At" },
-    { dataField: "id", text: "", formatter: this.removeFormatter },
-  ]
-
   render = () => (
     <Layout>
       <h2>Active Specs</h2>
 
-      <Table keyField="id" data={this.props.specifications} columns={this.specColumns} />
+      <Table keyField="id" data={this.props.specifications} columns={this.tableColumns} />
 
       <h3>Add a new spec</h3>
 
@@ -56,6 +39,33 @@ class Specify extends React.Component {
       <NewSpecForm onCreateSpecification={this.props.onCreateSpecification} />
     </Layout>
   )
+
+  removeFormatter = (cell, row) => (
+    <Button color="secondary" onClick={() => this.props.onRemoveSpec(cell)}>
+      Remove
+    </Button>
+  )
+
+  judgementFormatter = (cell, row) => (
+    <code>{cell}</code>
+  )
+
+  judgementHeader = (column, colIndex) => (
+    <span>
+    {column.text} (in <a href="https://www.ruby-lang.org/en/documentation/quickstart/">Ruby</a>)
+    </span>
+  )
+
+  tableColumns = [
+    { dataField: "partno", text: "Part" },
+    { dataField: "test_name", text: "Test" },
+    { dataField: "test_method", text: "Method" },
+    { dataField: "criteria", text: "Criteria" },
+    { dataField: "judgement", text: "Judgement Logic", headerFormatter: this.judgementHeader, formatter: this.judgementFormatter },
+    { dataField: "created_by", text: "Created By" },
+    { dataField: "created_at", text: "Created At" },
+    { dataField: "id", text: "", formatter: this.removeFormatter },
+  ]
 }
 
 const Layout = styled.div`
